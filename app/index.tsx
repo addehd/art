9dba2d1cec293b1fec528fbf32ae16045e27033a
addehd <hej@adolfo.se>
2026-02-28 20:59:38 +0100

    MVP

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,19 @@
 import { View, Text } from 'react-native';
+import { ViroARScene, ViroARSceneNavigator, ViroText } from '@viro-community/react-viro';
+
+function ARScene() {
+  return (
+    <ViroARScene>
+      <ViroText text="Hello World" />
+    </ViroARScene>
+  );
+}
+
 export default function Index() {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
-  )
+    <ViroARSceneNavigator
+      initialScene={{ scene: ARScene }}
+      style={{ flex: 1 }}
+    />
+  );
 }
